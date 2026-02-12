42a4199a684aaa4abed6396d794a4a7591f636ff
enoki-bit <enoki0215@gmail.com>
2026-02-12 22:39:10 +0900

    2/12 職場

---
+++ 自主回答/02/例題2/script-q2.js
@@ -0,0 +1,18 @@
+let score = 85;
+
+if(score >= 90)
+{
+    alert('A判定');
+}
+else if(score >= 80 && score < 90)
+{
+    alert('B判定');
+}
+else if(score >= 70 && score < 80)
+{
+    alert('C判定');
+}
+else
+{
+    alert('D判定');
+}
---
+++ 自主回答/02/例題3/script-q3.js
@@ -0,0 +1,28 @@
+let day = 6;
+
+switch(day)
+{
+    case 0:
+        alert('日曜日');
+        break;
+    case 1:
+        alert('月曜日');
+        break;
+    case 2:
+        alert('火曜日');
+        break;
+    case 3:
+        alert('水曜日');
+        break;
+    case 4:
+        alert('木曜日');
+        break;
+    case 5:
+        alert('金曜日');
+        break;
+    case 6:
+        alert('土曜日');
+        break;
+    default:
+        break;
+}
