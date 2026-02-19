69928653aba8fd31b43aa7b44b7de54cb034a9b6
enoki-bit <enoki0215@gmail.com>
2026-02-19 14:17:09 +0900

    2/19　自宅

---
+++ 自主回答/06/例題2/解答前/script-q2.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function()
+{
+    // id btnを使えるようにする
+    const btn = document.getElementById('btn');
+    // id cat_imgを使えるようにする
+    const cat_img = document.getElementById('cat_img');
+
+    // ボタンをクリックしたときの処理
+    btn.addEventListener('click', function()
+    {
+        // ここからどうするんだ？？？
+    });
+
+});
